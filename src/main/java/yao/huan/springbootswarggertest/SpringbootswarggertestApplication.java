bd2d59a90329b29f0c3930902226709d9b27637a
yaohuan <1515329308@qq.com>
2020-11-25 22:42:26 +0800

    ddd

---
+++ src/main/java/yao/huan/springbootswarggertest/SpringbootswarggertestApplication.java
@@ -0,0 +1,13 @@
+package yao.huan.springbootswarggertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootswarggertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootswarggertestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yao/huan/springbootswarggertest/SpringbootswarggertestApplicationTests.java
@@ -0,0 +1,13 @@
+package yao.huan.springbootswarggertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootswarggertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
